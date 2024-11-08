f09f47bb56f66da1cbb42b5796134729c1ac655b
Daniel Hess <d.hess@human.de>
2024-11-08 20:16:31 +0100

    chore: add configuration

--- CityInfo.Api/Program.cs
+++ CityInfo.Api/Program.cs
@@ -2,7 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -10,8 +13,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.UseEndpoints(endpoins =>
+{
+    endpoins.MapControllers();
+});
+app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+
 
 app.Run();
