b7f7e0a1a69508e475d3117b597aaffddf83ce04
Daniel Hess <d.hess@human.de>
2024-11-11 16:00:41 +0100

    feat: add manipulated problem details Responses

--- CityInfo.Api/Program.cs
+++ CityInfo.Api/Program.cs
@@ -2,7 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+    options.ReturnHttpNotAcceptable = true
+);
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = context =>
+    {
+        context.ProblemDetails.Extensions.Add("additionalInfo", "Additional info example");
+        context.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+    };
+});
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,10 +30,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
-app.UseEndpoints(endpoins =>
-{
-    endpoins.MapControllers();
-});
+app.UseEndpoints(endpoins => { endpoins.MapControllers(); });
 app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
 
 
